b031055fa05245177ac1eff6070d653316981bbf
TrebleSnake <treble.snake@yandex.ru>
2015-04-29 12:48:57 +0300

    Context root fix.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,3 +1,5 @@
+grails.app.context = '/'
+
 // locations to search for config files that get merged into the main config;
 // config files can be ConfigSlurper scripts, Java properties files, or classes
 // in the classpath in ConfigSlurper format
